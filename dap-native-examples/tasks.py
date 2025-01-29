81aed750bc5b69ce879f5c9d5a58489bb386759a
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-01-29 12:00:08 +0100

    build: switch to invoke

--- dap-native-examples/tasks.py
+++ dap-native-examples/tasks.py
@@ -2,6 +2,7 @@ from functools import wraps
 from pathlib import Path
 import cffi
 import glob
+import invoke
 import subprocess
 import tempfile
 
@@ -11,37 +12,47 @@ lib_folder = this_dir / "src" / "main" / "python" / "lib"
 def with_banner(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        text = func.__name__.replace("_", " ").title() + " task"
-        print("=" * len(text))
-        print(text)
-        print("=" * len(text))
+        header = "\n> " + func.__name__.replace("_", " ").title() + " task"
+        print(header)
+        print("‾" * len(header))
         return func(*args, **kwargs)
     return wrapper
 
+@invoke.task()
 @with_banner
-def clean():
-    subprocess.run([ "rm", "-rf", lib_folder ], check = True)
+def clean(c):
+    """
+    Clean up the Python `lib` folder.
+    """
+    invoke.run(f"rm -rf {lib_folder}")
 
+@invoke.task(pre=[clean])
 @with_banner
-def build_library():
-    subprocess.run([ "sbt", "dapNative/nativeLink" ], check = True, cwd = this_dir.parent)
+def build_library(c):
+    """
+    Build the DAP library in a shared object file and copy it to the Python `lib` folder.
+    """
+    invoke.run("cd .. && sbt dapNative/nativeLink")
     lib_files = glob.glob(str(this_dir.parent / "dap" / "native" / "target" / "scala-*" / "lib*"))
-    print("* Copying library to", lib_folder)
-    subprocess.run([ "mkdir", "-p", lib_folder ], check = True)
-    subprocess.run([ "cp", *lib_files, lib_folder ], check = True)
-    print("* Copying header file to", lib_folder)
+    log(f"Copying library to {lib_folder}")
+    invoke.run(f"mkdir -p {lib_folder}")
+    invoke.run(f"cp {" ".join(lib_files)} {lib_folder}")
+    log(f"Copying header file to {lib_folder}")
     h_files_paths = this_dir.glob("src/main/c/*.h")
     for h_file_path in h_files_paths:
         content = _pre_process(h_file_path)
         dest_path = Path(lib_folder) / h_file_path.name
         dest_path.write_text(content)
 
+@invoke.task(pre=[build_library])
 @with_banner
-def build_cffi():
-    build_library()
+def build_cffi(c):
+    """
+    Build the CFFI wrapper for the DAP library.
+    """
     ffi = cffi.FFI()
     h_file_name = lib_folder / "ctmc.h"
-    print("* Reading CFFI Declarations from", h_file_name)
+    log(f"Reading CFFI Declarations from {h_file_name}")
     with open(h_file_name) as h_file:
         lines = h_file.read().splitlines()
         lines += [
@@ -73,3 +84,6 @@ def _pre_process(header_file_path: str) -> str:
         subprocess.run(["clang", "-E", "-P", header_file_path, "-o", out_path], check=True)
         with open(out_path, "r") as f:
             return f.read()
+
+def log(msg: str):
+    print(f"[info] {msg}")
