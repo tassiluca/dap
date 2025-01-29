a54d9e069764e39610c09af2b73231d6534cf886
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-01-29 11:23:46 +0100

    build: use prepoccesing phase

--- dap-native-examples/scripts.py
+++ dap-native-examples/scripts.py
@@ -1,49 +1,44 @@
-import pathlib
-import cffi
-import subprocess
-import re
-import glob
-import pathlib
+from functools import wraps
 from pathlib import Path
+import cffi
+import glob
+import subprocess
+import tempfile
 
-this_dir = pathlib.Path().resolve()
+this_dir = Path().resolve()
 lib_folder = this_dir / "src" / "main" / "python" / "lib"
 
-def print_banner(text):
-    print("=" * len(text))
-    print(text)
-    print("=" * len(text))
+def with_banner(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        text = func.__name__.replace("_", " ").title() + " task"
+        print("=" * len(text))
+        print(text)
+        print("=" * len(text))
+        return func(*args, **kwargs)
+    return wrapper
 
+@with_banner
 def clean():
-    print_banner("Cleaning")
     subprocess.run([ "rm", "-rf", lib_folder ], check = True)
 
-def clean_header(content):
-    content = re.sub(r'^\s*#ifndef.*\n\s*#define.*\n', '', content, count=1, flags=re.MULTILINE)
-    content = re.sub(r'\n\s*#endif.*$', '', content, flags=re.MULTILINE)
-    return content
-
+@with_banner
 def build_library():
-    print_banner("Building Native Library")
-    print("* Compiling library")
     subprocess.run([ "sbt", "dapNative/nativeLink" ], check = True, cwd = this_dir.parent)
     lib_files = glob.glob(str(this_dir.parent / "dap" / "native" / "target" / "scala-*" / "lib*"))
     print("* Copying library to", lib_folder)
     subprocess.run([ "mkdir", "-p", lib_folder ], check = True)
     subprocess.run([ "cp", *lib_files, lib_folder ], check = True)
     print("* Copying header file to", lib_folder)
-    h_files = this_dir.glob("src/main/c/*.h")
-    for h_file in h_files:
-        # cffi doesn't support `#ifndef` declarations in headers :(
-        content = h_file.read_text()
-        cleaned_content = clean_header(content)
-        dest_path = Path(lib_folder) / h_file.name
-        dest_path.write_text(cleaned_content)
-    print("* Complete")
+    h_files_paths = this_dir.glob("src/main/c/*.h")
+    for h_file_path in h_files_paths:
+        content = _pre_process(h_file_path)
+        dest_path = Path(lib_folder) / h_file_path.name
+        dest_path.write_text(content)
 
+@with_banner
 def build_cffi():
     build_library()
-    print_banner("Building CFFI Module")
     ffi = cffi.FFI()
     h_file_name = lib_folder / "ctmc.h"
     print("* Reading CFFI Declarations from", h_file_name)
@@ -69,4 +64,12 @@ def build_cffi():
         runtime_library_dirs=[lib_folder.as_posix()],
     )
     ffi.compile(tmpdir=lib_folder.as_posix())
-    print("* Complete")
+
+# !Warning! cffi does not support C preprocessor directives. This function pre-processes the header file to remove them.
+def _pre_process(header_file_path: str) -> str:
+    with tempfile.NamedTemporaryFile(suffix=".i", delete=True) as temp_file:
+        out_path = temp_file.name
+        # Run the preprocessor on the header file (-E) suppressing line markers (-P) directives
+        subprocess.run(["clang", "-E", "-P", header_file_path, "-o", out_path], check=True)
+        with open(out_path, "r") as f:
+            return f.read()
