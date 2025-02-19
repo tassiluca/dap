e4152150e655c98982d18c167859c18350975de9
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-02-19 10:32:34 +0100

    fix: use last event.state in place of state

--- dap-jvm-examples/src/main/scala/it/unibo/dap/examples/DAPGossip.scala
+++ dap-jvm-examples/src/main/scala/it/unibo/dap/examples/DAPGossip.scala
@@ -12,21 +12,35 @@ object DAPGossip:
 
   private val gossipRules = DAP(
     Rule(MSet("a", "a"), _ => 1_000, MSet("a"), ""), // a|a --1000--> a
-    Rule(MSet("a"), _ => 0.5, MSet("a"), "a"), // a --1--> a|^a
+    Rule(MSet("a"), _ => 1, MSet("a"), "a"), // a --1--> a|^a
   )
 
   val gossipCTMC: CTMC[State] = DAP.toCTMC(gossipRules)
 
-@main def leftNode(): Unit = Async.blocking:
+@main def leftUpNode(): Unit = Async.blocking:
   import DAPGossip.*
-  val simulation = DistributedSimulation.create(port = 2550, network = Set(("localhost", 2551)))
+  val simulation = DistributedSimulation.create(port = 2550, network = Set(("localhost", 2551), ("localhost", 2552)))
   scribe.info("Wating 5 seconds")
   AsyncOperations.sleep(5.seconds)
   simulation.of(State(MSet("a"), ""), gossipCTMC)
 
-@main def rightNode(): Unit = Async.blocking:
+@main def rightUpNode(): Unit = Async.blocking:
   import DAPGossip.*
-  val simulation = DistributedSimulation.create(port = 2551, network = Set(("localhost", 2550)))
-  scribe.info("Wating 5 seconds")
+  val simulation = DistributedSimulation.create(port = 2551, network = Set(("localhost", 2550), ("localhost", 2553)))
+  scribe.info("Wating 4 seconds")
+  AsyncOperations.sleep(5.seconds)
+  simulation.of(State(MSet(), ""), gossipCTMC)
+
+@main def leftBtmNode(): Unit = Async.blocking:
+  import DAPGossip.*
+  val simulation = DistributedSimulation.create(port = 2552, network = Set(("localhost", 2550), ("localhost", 2553)))
+  scribe.info("Wating 3 seconds")
+  AsyncOperations.sleep(5.seconds)
+  simulation.of(State(MSet(), ""), gossipCTMC)
+
+@main def rightBtmNode(): Unit = Async.blocking:
+  import DAPGossip.*
+  val simulation = DistributedSimulation.create(port = 2553, network = Set(("localhost", 2551), ("localhost", 2552)))
+  scribe.info("Wating 2 seconds")
   AsyncOperations.sleep(5.seconds)
   simulation.of(State(MSet(), ""), gossipCTMC)
--- dap/shared/src/main/scala/it/unibo/dap/core/DistributedSimulation.scala
+++ dap/shared/src/main/scala/it/unibo/dap/core/DistributedSimulation.scala
@@ -1,13 +1,13 @@
 package it.unibo.dap.core
 
 import gears.async.TaskSchedule.RepeatUntilFailure
-import gears.async.{Async, AsyncOperations, Future, Task}
+import gears.async.{ Async, AsyncOperations, Future, Task }
 import it.unibo.dap.modelling.DAP
 import it.unibo.dap.modelling.Simulatable
 import it.unibo.dap.utils.MSet
 
 import java.util.Random
-import java.util.concurrent.{BlockingDeque, LinkedBlockingDeque}
+import java.util.concurrent.{ BlockingDeque, LinkedBlockingDeque }
 import scala.annotation.tailrec
 import scala.concurrent.duration.DurationDouble
 
@@ -19,6 +19,7 @@ class DistributedSimulation(boundary: Exchange[String]):
   )(using Async, AsyncOperations): Unit = Async.group:
     val queue = LinkedBlockingDeque[String]()
     val all = Task(boundary.inputs.read().foreach(queue.add)).schedule(RepeatUntilFailure()).start() ::
+      Future(watchDog(queue)) ::
       Future(loop(queue, behavior, initial)) ::
       Nil
     all.awaitAll
@@ -29,17 +30,18 @@ class DistributedSimulation(boundary: Exchange[String]):
       behavior: F[DAP.State],
       state: DAP.State,
   )(using Async.Spawn, AsyncOperations): Unit =
-    scribe.info("Simulating step")
     val event = behavior.simulateStep(state, new Random())
-    scribe.info(s"Event @ ${event.time}: ${event.state}")
     AsyncOperations.sleep(event.time.seconds)
-    scribe.info("Ended waiting")
     boundary.outputs.send(event.state.msg)
     val in = Option(queue.poll())
-    scribe.info(s"Received message: $in")
-    val newState = in.fold(event.state)(t => DAP.State(state.local union MSet(t), ""))
+    val newState = in.fold(event.state)(t => DAP.State(event.state.local union MSet(t), ""))
     scribe.info(s"New state: $newState")
     loop(queue, behavior, newState)
+
+  private def watchDog(queue: BlockingDeque[String])(using Async, AsyncOperations): Unit =
+    while true do
+      AsyncOperations.sleep(5.second)
+      scribe.info(s"[WatchDog] Accumulated msgs: ${queue.size()}")
 
 end DistributedSimulation
 
@@ -48,7 +50,10 @@ object DistributedSimulation:
   import it.unibo.dap.sockets.SocketExchange
   import SocketExchange.*
 
-  def create[T](port: Port, network: Set[SocketExchange.Endpoint])(using Async.Spawn, AsyncOperations): DistributedSimulation =
+  def create[T](port: Port, network: Set[SocketExchange.Endpoint])(using
+      Async.Spawn,
+      AsyncOperations,
+  ): DistributedSimulation =
     val exchange = SocketExchange(port, network)
     exchange.start
     DistributedSimulation(exchange)
