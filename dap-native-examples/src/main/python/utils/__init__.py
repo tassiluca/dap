aaf2d7fc4a735b36c5deabaf18b4d8f06de4b3fb
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-03-28 10:50:19 +0100

    refactor: remove cffi wrappers

--- dap-native-examples/src/main/python/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from lib import dap_cffi
-ffi = dap_cffi.ffi
-
-def addr(cstruct):
-    """
-    Return the hexadecimal address of a cstruct.
-    """
-    return hex(int(ffi.cast("uintptr_t", cstruct)))
