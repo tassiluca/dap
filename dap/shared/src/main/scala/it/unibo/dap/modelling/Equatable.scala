e64e8ac43ff35abbdb56fa4abb15adf7d9e13b09
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-04-08 18:41:10 +0200

    fix(modelling): Equatable reference correctly to their function and not the equals on Object

--- dap/shared/src/main/scala/it/unibo/dap/modelling/Equatable.scala
+++ dap/shared/src/main/scala/it/unibo/dap/modelling/Equatable.scala
@@ -5,4 +5,4 @@ trait Equatable[T] extends ((T, T) => Boolean)
 object Equatable:
   def apply[T](using Equatable[T]): Equatable[T] = summon[Equatable[T]]
 
-extension [T](t1: T) def ===(t2: T)(using Equatable[T]): Boolean = Equatable[T].equals(t1, t2)
+extension [T](t1: T) def ===(t2: T)(using eq: Equatable[T]): Boolean = eq(t1, t2)
